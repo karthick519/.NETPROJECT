bf0b5ce8bd121a37f661905e4516e01b4f5dd0f3
karthick519 <karthickhackr8@gmail.com>
2022-12-08 11:01:23 +0530

    firstcommit

---
+++ SampleApp.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace SampleApp.Web.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
